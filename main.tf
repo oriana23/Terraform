1f69c6a973d03c6e329b419bbf5387b8e6f4167f
Oriana <aleja061213@gmail.com>
2026-02-04 00:26:30 -0300

    output

--- main.tf
+++ main.tf
@@ -5,5 +5,94 @@ provider "aws" {
 resource  "aws_instance" "nginx-server" {
   ami           = "ami-0532be01f26a3de55"
   instance_type = "t3.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y nginx
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+
+            EOF
+
+key_name = aws_key_pair.nginx-server-ssh.key_name
+
+vpc_security_group_ids = [
+  aws_security_group.nginx-server-sg.id
+ ]
+
+ tags = {
+   Name= "nginx-server"
+   Environment = "test"
+   Owner = "oria.ramirez@outlook.com"
+   Team: "DevOps"
+   Project = "Webinar"
+
 }
 
+}
+
+# Generar clave ssh  sh-keygen -t rsa -b 2048 -f "nginx-server.key"
+
+resource "aws_key_pair" "nginx-server-ssh" {
+  key_name = "nginx-server-ssh"
+  public_key = file("nginx-server.key.pub")
+
+  tags = {
+   Name= "nginx-server-ssh"
+   Environment = "test"
+   Owner = "oria.ramirez@outlook.com"
+   Team: "DevOps"
+   Project = "Webinar"
+  
+}
+
+}
+
+
+resource "aws_security_group" "nginx-server-sg" {
+  name = "nginx-server-sg"
+  description = "security group allowing SSH and HTTP access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+   Name = "nginx-server-sg"
+   Environment = "test"
+   Owner = "oria.ramirez@outlook.com"
+   Team: "DevOps"
+   Project = "Webinar"
+  
+}
+
+}
+
+output "server_publib_ip" {
+  description = " Direccion IP publica de la instancia EC2"
+  value = aws_instance.nginx-server.public_ip
+  
+}
+
+output "server_publib_dns" {
+  description = "DNS publico de la instancia EC2"
+  value = aws_instance.nginx-server.public_dns
+  
+}
